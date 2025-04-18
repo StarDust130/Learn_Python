e7f4f66d3cff0958f1dba7394483de88f1a8315e
ChandraShekhar <csyadav0513@gmail.com>
2025-04-18 09:33:55 +0530

    Add grade calculation script to assign letter grades based on student scores

---
+++ 02_conditionals/grade_cal.py
@@ -0,0 +1,16 @@
+# Problem: Assign a letter grade based on a student's score: A (90-100), B (80-89), C (70-79), D (60-69), F (below 60).
+
+score = int(input("Enter score: "))
+
+if score >= 90:
+    grade = "A"
+elif score >= 80:
+    grade = "B"
+elif score >= 70:
+   grade = "C"
+elif score >= 60:
+    grade = "D"
+else:
+   grade = "F"
+
+print(f"Your score is {score}% Grade: {grade}")
