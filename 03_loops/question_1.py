090a731e974ae93da3c81b55f02ee03836b9c81a
ChandraShekhar <csyadav0513@gmail.com>
2025-04-25 10:01:16 +0530

    Add script to count positive numbers in a list

---
+++ 03_loops/question_1.py
@@ -0,0 +1,14 @@
+# Problem: Given a list of numbers, count how many are positive.
+
+
+numbers = [1, -2, 3, -4, 5, 6, -7, -8, 9, 10, 5]
+
+count = 0
+
+for i in numbers:
+    if i >= 0:
+        count += 1
+
+print(f"Postive Number in List is {count}")
+
+
