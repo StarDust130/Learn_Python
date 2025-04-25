3683399afa9bf8c25685de743263077632cf3f2e
ChandraShekhar <csyadav0513@gmail.com>
2025-04-25 10:15:17 +0530

    Add script to calculate the sum of even numbers up to a given number

---
+++ 03_loops/question_2.py
@@ -0,0 +1,14 @@
+# Problem: Calculate the sum of even numbers up to a given number n.
+
+n = int(input("Enter a Number: "))
+
+sum_even = 0
+
+for i in range(1, n + 1):
+    if i%2 == 0:
+        sum_even += 1
+
+print(f"Sum even is {sum_even}")
+
+      
+
