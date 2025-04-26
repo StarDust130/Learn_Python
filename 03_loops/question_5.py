c176805ac21cdc7a98065a3243a5e15c071d64ec
ChandraShekhar <csyadav0513@gmail.com>
2025-04-26 12:49:47 +0530

    Add script to find the first non-repeated character in a string

---
+++ 03_loops/question_5.py
@@ -0,0 +1,10 @@
+# Given a string, find the first non-repeated character.
+
+# input_str = str(input("Enter your dost ka Naam: "))
+
+input_str = "aauuuk"
+
+for char in input_str:
+    if input_str.count(char) == 1:
+        print(f"Charcter is {char}")
+
