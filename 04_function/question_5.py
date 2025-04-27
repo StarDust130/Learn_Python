5ac9e040d43c441aca34b1d618d59e44ded728e0
ChandraShekhar <csyadav0513@gmail.com>
2025-04-27 10:16:49 +0530

    Add greeting function with default name parameter

---
+++ 04_function/question_5.py
@@ -0,0 +1,8 @@
+#  Problem: Write a function that greets a user. If no name is provided, it should greet with a default name.
+
+def greets(name="User"):
+
+    print("Hello", name)
+
+
+greets("Cake")
