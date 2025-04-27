f2d5035776af8ed4a31733a31675af931399a0b5
ChandraShekhar <csyadav0513@gmail.com>
2025-04-27 10:09:55 +0530

    Add function to multiply two numbers and handle string multiplication

---
+++ 04_function/question_3.py
@@ -0,0 +1,10 @@
+# Problem: Write a function multiply that multiplies two numbers, but can also accept and multiply strings.
+
+def multiply(num1 , num2):
+    return num1 * num2
+
+
+result = multiply("cake" ,2)
+print(result)
+
+
