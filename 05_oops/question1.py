c1378795df66a1679c3e6f1815a9b7fdb6b28078
ChandraShekhar <csyadav0513@gmail.com>
2025-05-03 10:52:05 +0530

    Refactor Car class: rename __str__ method to full_name and update print statement

--- 05_oops/question1.py
+++ 05_oops/question1.py
@@ -3,10 +3,13 @@ class Car:
         self.brand = brand
         self.model = model
 
+    def full_name(self):
+        return f"{self.brand} {self.model}"
+
     def __str__(self):  # magic method to print nicely
         return f"{self.brand} {self.model}"
 
-
 my_car = Car("Mahindra", "BE 09")
-print(my_car)  # Now prints: Mahindra BE 09
+print(my_car.full_name())  # Now prints: Mahindra BE 09
+
 
