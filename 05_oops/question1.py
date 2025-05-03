19b2cf5e1a0bc9c6a1a7662faf4a5a539add82b5
ChandraShekhar <csyadav0513@gmail.com>
2025-05-03 12:28:41 +0530

    Refactor Car class: add total_car attribute and general_description method for better tracking and description

--- 05_oops/question1.py
+++ 05_oops/question1.py
@@ -1,7 +1,9 @@
 class Car:
+    total_car = 0
     def __init__(self, brand, model):
         self.__brand = brand
         self.model = model
+        Car.total_car += 1
 
     def get_brand(self):
         return self.__brand
@@ -9,6 +11,12 @@ class Car:
 
     def full_name(self):
         return f"{self.__brand} {self.model}"
+    
+    @staticmethod
+    def general_description():
+        return "Cars are Cool 😼"
+
+    
 
     def __str__(self):
         return f"{self.__brand} {self.model}"
@@ -27,14 +35,34 @@ class ElectricCar(Car):
         parent_str = super().__str__()
         return f"{parent_str} | Battery: {self.battery_size}"
 
-      
+
+class Battery:
+    def batter_info(self):
+        return " Battery 🙂"
+
+class Engine:
+    def engine_info(self):
+        return " Engine 😆"
 
 
+class ElectricCarTwo(Battery , Engine , Car):
+    pass
+
+
+
+
+my_tesla = ElectricCarTwo("tesla" , "Y")
+
+print(my_tesla.engine_info())
 
 # my_car = Car("Mahindra", "BE 09")
-# print(my_car.__brand)  
+
+# print(my_car.general_description())
 
 
-my_ev_car = ElectricCar("BMW", " i7", "101.7 kWh")
 
-print(my_ev_car.get_brand())
+# my_ev_car = ElectricCar("BMW", " i7", "101.7 kWh")
+
+# print(my_ev_car.get_brand())
+
+# print(Car.total_car)
