14091e0392419b19f09a7c7a9c32507bc9ab9741
ChandraShekhar <csyadav0513@gmail.com>
2025-05-05 08:56:53 +0530

    Refactor video management functions and improve main loop structure

--- 07_yt_manager/youtube_manager.py
+++ 07_yt_manager/youtube_manager.py
@@ -1,31 +1,46 @@
 
-
+# (1)
 def list_all_videos(videos):
     print("\n")
     print("*" * 70)
 
+# (2)
+def add_video(videos):
+     pass
+
+# (3)
+def update_video(videos):
+     pass
+
+# (4)
+def delete_video(videos):
+     pass
+
+def main ():
+    videos = []
+    while True:
+        print("\n Youtube Manager | choose an option ")
+        print("1. List all youtube videos ")
+        print("2. Add a youtube video ")
+        print("3. Update a youtube video details ")
+        print("4. Delete a youtube video ")
+        print("5. Exit the app ")
+        choice = str(input("Enter your choice: "))
 
 
-
-
-while True:
-    print("\n Youtube Manager | choose an option ")
-    print("1. List all youtube videos ")
-    print("2. Add a youtube video ")
-    print("3. Update a youtube video details ")
-    print("4. Delete a youtube video ")
-    print("5. Exit the app ")
-    choice = str(input("Enter your choice: "))
-
-    match choice:
-        case "1":
-            list_all_videos(choice)
-        case "2":
-            pass
-        case "3":
-            pass
-        case "4":
-            pass
-        case "5":
-            print("Have a Nice Day! Bye 🍼")
-            exit()
+        match choice:
+            case '1':
+                    list_all_videos(videos)
+            case '2':
+                add_video(videos)
+            case '3':
+                update_video(videos)
+            case '4':
+                delete_video(videos)  
+            case "5":
+                print("Have a Nice Day! Bye 🍼")
+                break
+            case _:
+                    print("Invalid Choice 😠")
+            
+            
