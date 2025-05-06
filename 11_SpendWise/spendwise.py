87d2139e2f1d208d8156ac53ae6a59412b271077
ChandraShekhar <csyadav0513@gmail.com>
2025-05-06 10:09:12 +0530

    Add SpendWise project description and purpose to spendwise.py

---
+++ 11_SpendWise/spendwise.py
@@ -0,0 +1,7 @@
+"""
+📝 SpendWise – Smart Money Tracker 💸
+SpendWise helps you 💰 track your income, 🧾 record expenses, 🎯 set budgets, and 📊 see where your money goes – all from your terminal.
+
+Stay in control of your cash, spend wisely. 😉
+"""
+
