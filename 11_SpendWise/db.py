10b05e042aadac0376eda6f57c1687c3e0331cdd
ChandraShekhar <csyadav0513@gmail.com>
2025-05-07 13:15:36 +0530

    Update MongoDB connection string to use the correct URI format

--- 11_SpendWise/db.py
+++ 11_SpendWise/db.py
@@ -2,7 +2,7 @@
 from pymongo import MongoClient
 
 client = MongoClient(
-    "Mongo_DB_URL")
+    "mongodb+srv://stardut:LNRHe9WYRPGe5TcE@cluster0.hfaghq6.mongodb.net/")
 
 db = client["spendwise"]
 
